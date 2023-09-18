faf6f05a6ff9c49c1c7fbfc351a9a49f92e54914
lostRating <rauledly@gmail.com>
2023-09-18 09:05:03 +0800

    Revert "fix import"
    This reverts commit 0ad7c3e83723db3b5771e827166b0ca4727755b6.

--- contracts/UniswapV2Factory.sol
+++ contracts/UniswapV2Factory.sol
@@ -1,7 +1,6 @@
 pragma solidity =0.6.6;
 
-import '@uniswap/v2-core/contracts/interfaces/IUniswapV2Factory.sol';
-import '@uniswap/v2-core/contracts/interfaces/IUniswapV2Pair.sol';
+import './interfaces/IUniswapV2Factory.sol';
 import './UniswapV2Pair.sol';
 
 contract UniswapV2Factory is IUniswapV2Factory {
